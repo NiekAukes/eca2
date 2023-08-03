8985a9313c0e9949da731d5ff8df8fded4f7dfe6
Niek Aukes <Niek.aukes@gmail.com>
2023-08-03 22:50:09 +0200

    remove build.bat

--- build.bat
+++
@@ -1,3 +0,0 @@
-rmdir /s /q dist
-py -m build
-py -m twine upload --repository pypi dist/* --username __token__ --password pypi-AgEIcHlwaS5vcmcCJDE3ZjJjYTNmLTYyZjYtNDgzNi1iN2MyLTk0MzQzMmFlOTEyOAACKlszLCI3MWZlNzlhNy1kMDBjLTQ3YmYtYmRhMC0xZmJmMjIxNjlhMDciXQAABiBpPFkgMfXyDilnFfPZbpKZcx_sDm3vcpxObNbsuCP1fg
