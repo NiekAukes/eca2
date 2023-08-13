a1f53f35ad094f7eb12204531d84347b25788616
Niek Aukes <Niek.aukes@gmail.com>
2023-08-13 20:59:11 +0200

    update command line with unified templates

--- neca/__main__.py
+++ neca/__main__.py
@@ -28,6 +28,17 @@ tutorial_names = {
 tutorials = {path.absolute(): tutorial_names[path.name] for path in (path / "tutorials").iterdir() if path.is_dir()}
 templates.update(tutorials)
 
+demo_names = {
+    "bar_chart_example": "Bar Chart Example",
+    "connector_example": "Connector Example",
+    "form_example": "Form Example",
+    "line_chart_example": "Line Chart Example",
+    "pie_chart_example": "Pie Chart Example",
+    "linear_chart_example": "Linear Chart Example"
+}
+demos = {path.absolute(): demo_names[path.name] for path in (path / "demos").iterdir() if path.is_dir()}
+templates.update(demos)
+
 
 
 
@@ -65,70 +76,8 @@ def create(project_name: str = ""):
 
     rprint(f"[green]project '{project_name}' created[/green]")
 
-#@cli.command()
-def tutorial():
-    #print("use the create command to create a new tutorial project")
-    pass
 
-demos = {path.name: path.absolute() for path in (path / "demos").iterdir() if path.is_dir()}
-demo_names = {
-    "D1": "Demo 1: The Event System",
-    "D2": "Demo 2: Creating your first Block",
-}
-
-@cli.command()
-def demo(path: str = ""):
-    """
-    creates a new folder with a demo for you to play with
-    """
-
-
-    # ask the user what demo name to use
-    names = [demo_names[demo] for demo in demos.keys()]
-    prompt = ListPrompt(message="what demo do you want to run?",
-                        choices=names,
-                        pointer=">"
-                        )
-    demo_name = prompt.execute()
-
-    # convert the demo name to the path
-    real_demo = list(demo_names.keys())[list(demo_names.values()).index(demo_name)]
-    demo_path = demos[real_demo]
-
-    
-
-    # if the path is not specified, ask the user what path to use
-    if path == "":
-        questions = [
-            {
-                'type': 'input',
-                'name': 'path',
-                'message': 'what is the path of the demo?',
-                'default': 'my_demo'
-            }
-        ]
-        
-        prompt = FilePathPrompt(
-            message="which path do you want to use?",
-            default="my_demo"
-        )
-
-        path = prompt.execute()
-        
-    
-
-    # copy the demo to the current directory
-    shutil.copytree(demo_path, path)
-
-    rprint(f"[green]demo '{demo_name}' created[/green]")
-    
-
-
-@cli.command()
-def ship():
-    print("All hands on deck!")
-
-
+"""
 @cli.callback(invoke_without_command=True)
 def no_command(ctx: typer.Context):
     # default action, make user choose a command
@@ -163,6 +112,6 @@ def no_command(ctx: typer.Context):
         ctx.invoke(tutorial)
     elif command == "show a demo":
         ctx.invoke(demo)
-
+"""
 if __name__ == '__main__':
     cli()
